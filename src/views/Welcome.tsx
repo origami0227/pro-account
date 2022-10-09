1afc31f54779b5c446287f3c5fdc67689ad1e45e
origami <1071242996@qq.com>
2022-10-09 15:15:34 +0800

    feature:初始化css

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,11 +1,20 @@
 import { defineComponent } from 'vue'
 import { RouterView } from 'vue-router'
+import s from './Welcome.module.scss'
+import logo from '../assets/icons/mangosteen.svg'
 export const welcome = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-        <Router-view />
-      </div>
+        <div class={s.wrapper}>
+          <header>
+              <img src={logo} />
+              <h1>超梦记账</h1>
+          </header>
+          <main>
+            <RouterView />
+          </main>
+        </div>
+
     )
   },
 })
