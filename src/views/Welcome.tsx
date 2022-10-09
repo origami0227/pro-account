6a911f45bc1f0d7558f3147bb1b646922b2abc40
origami <1071242996@qq.com>
2022-10-09 18:18:21 +0800

    feature:更新svg以及主题

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,14 +1,14 @@
 import { defineComponent } from 'vue'
 import { RouterView } from 'vue-router'
 import s from './Welcome.module.scss'
-import logo from '../assets/icons/mangosteen.svg'
+import logo from '../assets/icons/kabi.svg'
 export const welcome = defineComponent({
   setup: (props, context) => {
     return () => (
         <div class={s.wrapper}>
           <header>
-              <img src={logo} />
-              <h1>超梦记账</h1>
+              <img class={s.logo} src={logo} />
+              <h1>卡比记账</h1>
           </header>
           <main class={s.main}>
             <RouterView />
