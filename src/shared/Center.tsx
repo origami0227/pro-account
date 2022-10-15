6b3e2a36d33deedbc4bb35ab6b7328803d41b6d2
origami <1071242996@qq.com>
2022-10-15 13:02:29 +0800

    feature:制作center组件

---
+++ src/shared/Center.tsx
@@ -0,0 +1,27 @@
+import {defineComponent, PropType} from "vue";
+import s from './Center.module.scss'
+
+const directionMap = {
+    '-': 'horizontal',
+    '|': 'vertical',
+    'horizontal': 'horizontal',
+    'vertical': 'vertical'
+}
+export const Center = defineComponent({
+    props: {
+        direction: {
+            type: String as PropType<'-' | '|' | 'horizontal' | 'vertical'>,
+            default: 'horizontal'
+        }
+    },
+    setup: (props, context) => {
+        // const extraClass = props.direction === '-' || props.direction === 'horizontal' ?
+        //     'horizontal' : 'vertical'
+        const extraClass = directionMap[props.direction]
+        return () => (
+            <div class={[s.center, extraClass]}>
+                {context.slots.default?.()}
+            </div>
+        )
+    }
+})
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,19 +1,25 @@
-import {defineComponent} from "vue";
-import {Button} from "../shared/Button";
-import s from './StartPage.module.scss'
-import {FloatButton} from "../shared/FloatButton";
+import {defineComponent} from 'vue';
+import {Button} from '../shared/Button';
+import {Center} from '../shared/Center';
+import {FloatButton} from '../shared/FloatButton';
+import {Icon} from '../shared/Icon';
+import s from './StartPage.module.scss';
 
 export const StartPage = defineComponent({
     setup: (props, context) => {
-        const onClick = ()=>{
-            console.log('click')
+        const onClick = () => {
+            console.log('hi')
         }
         return () => (
             <div>
+                <nav>menu</nav>
+                <Center class={s.pig_wrapper}>
+                    <Icon name="pig" class={s.pig}/>
+                </Center>
                 <div class={s.button_wrapper}>
-                    <Button class={s.button} onClick={onClick}>test</Button>
+                    <Button class={s.button} onClick={onClick}>测试</Button>
                 </div>
-                <FloatButton iconName="add" />
+                <FloatButton iconName='add'/>
             </div>
         )
     }
