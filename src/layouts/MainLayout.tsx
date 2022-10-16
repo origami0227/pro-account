8bcc9aecc09f166fd895032661145dc8dad45576
origami <1071242996@qq.com>
2022-10-16 13:56:51 +0800

    抽离MainLayout组件布局

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,20 @@
+import {defineComponent, PropType} from "vue";
+import {Navbar} from "../shared/Navbar";
+
+
+export const MainLayout = defineComponent({
+
+    setup: (props, context) => {
+        return () => (
+            <div>
+                <Navbar>{
+                    {
+                        default: () => context.slots.title?.(),
+                        icon: () => context.slots.icon?.()
+                    }
+                }</Navbar>
+                {context.slots.default?.()}
+            </div>
+        )
+    }
+})
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -7,6 +7,7 @@ import s from './StartPage.module.scss';
 import {Navbar} from '../shared/Navbar';
 import {Overlay} from "../shared/Overlay";
 import {RouterLink} from "vue-router";
+import {MainLayout} from "../layouts/MainLayout";
 
 
 export const StartPage = defineComponent({
@@ -16,29 +17,32 @@ export const StartPage = defineComponent({
             refOverlayVisible.value = !refOverlayVisible.value
         }
         return () => (
-            <div>
-                <Navbar>{
-                    {
-                        default: () => '卡比记账',
-                        icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu}/>
-                    }
-                }</Navbar>
-                <Center class={s.pig_wrapper}>
-                    <Icon name="pig" class={s.pig}/>
-                </Center>
-                <div class={s.button_wrapper}>
-                    <RouterLink to="/items/create">
-                        <Button class={s.button}>开始记账</Button>
-                    </RouterLink>
 
-                </div>
-                <RouterLink to="/items/create">
-                    <FloatButton iconName='add'/>
-                </RouterLink>
-                {refOverlayVisible.value &&
-                    <Overlay onClose={() => refOverlayVisible.value = false}/>//如果overLayVisible的value存在才会展示。 点击触发让value取反
+            <MainLayout>{
+                {
+                    title: () => '卡比记账',
+                    icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu}/>,
+                    default: () => (<>
+                            <Center class={s.pig_wrapper}>
+                                <Icon name="pig" class={s.pig}/>
+                            </Center>
+                            <div class={s.button_wrapper}>
+                                <RouterLink to="/items/create">
+                                    <Button class={s.button}>开始记账</Button>
+                                </RouterLink>
+
+                            </div>
+                            <RouterLink to="/items/create">
+                                <FloatButton iconName='add'/>
+                            </RouterLink>
+                            {refOverlayVisible.value &&
+                                <Overlay onClose={() => refOverlayVisible.value = false}/>//如果overLayVisible的value存在才会展示。 点击触发让value取反
+                            }
+                        </>
+                    )
                 }
-            </div>
+            }</MainLayout>
+
         )
     }
 })
