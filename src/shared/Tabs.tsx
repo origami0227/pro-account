a53610f561ea9f93a9339f4cf77300bc1acf6516
origami <1071242996@qq.com>
2022-10-24 13:59:08 +0800

    支持classPrefix

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -1,31 +1,41 @@
-import {defineComponent, PropType} from 'vue';
+import { defineComponent, PropType } from 'vue';
 import s from './Tabs.module.scss';
-
 export const Tabs = defineComponent({
     props: {
+        classPrefix: {
+            type: String
+        },
         selected: {
             type: String as PropType<string>,
-            required: false
+            required: false,
         },
+        onUpdateSelected: {
+            type: Function as PropType<(name: string) => void>,
+            required: false,
+        }
     },
     setup: (props, context) => {
         return () => {
             const tabs = context.slots.default?.()
             if (!tabs) return () => null
             for (let i = 0; i < tabs.length; i++) {
-                if (tabs[i].type != Tab) {
+                if (tabs[i].type !== Tab) {
                     throw new Error('<Tabs> only accepts <Tab> as children')
                 }
             }
-            return <div class={s.tabs}>
-                <nav>
-                    <ol class={s.tabs_nav}>
-                        {tabs.map(item => <li
-                            class={item.props?.name === props.selected ? s.selected : ''}
+            const cp = props.classPrefix
+            return <div class={[s.tabs, cp + '_tabs']}>
+                <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
+                    {tabs.map(item =>
+                        <li class={[
+                            item.props?.name === props.selected ? [s.selected, cp + '_selected'] : '',
+                            cp + '_tabs_nav_item'
+                        ]}
                             onClick={() => context.emit('update:selected', item.props?.name)}
-                        >{item.props?.name}</li>)}
-                    </ol>
-                </nav>
+                        >
+                            {item.props?.name}
+                        </li>)}
+                </ol>
                 <div>
                     {tabs.find(item => item.props?.name === props.selected)}
                 </div>
@@ -33,6 +43,7 @@ export const Tabs = defineComponent({
         }
     }
 })
+
 export const Tab = defineComponent({
     props: {
         name: {
