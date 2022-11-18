2e9b2ce85bc3653b262ca398df593cf5c9b27ed5
origami <1071242996@qq.com>
2022-11-18 15:14:55 +0800

    feature:登录检查，添加请求拦截器

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -17,6 +17,7 @@ import {TagCreate} from "../component/tag/TagCreate";
 import {TagEdit} from "../component/tag/TagEdit";
 import {SignInPage} from "../views/SignInPage";
 import {StatisticsPage} from "../views/StatisticsPage";
+import { http } from "../shared/Http";
 
 export const routes: RouteRecordRaw[] = [
     {path: '/', redirect: '/welcome'},
@@ -39,6 +40,12 @@ export const routes: RouteRecordRaw[] = [
     },
     {
         path: '/items', component: ItemPage,
+        beforeEnter: async (to, from, next) => {
+            await http.get('/me').catch(() => {
+                next('/sign_in?return_to=' + to.path)
+            })
+            next()
+        },//获取当前用户信息，如果成功则正常继续，如果失败则重新定向到登录注册界面
         children: [
             {path: '', component: ItemList},
             {path: 'create', component: ItemCreate},
