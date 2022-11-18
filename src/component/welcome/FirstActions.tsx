6a64a929a5fcb17413c09d58cd73e06ec1b91413
origami <1071242996@qq.com>
2022-11-18 14:52:03 +0800

    feature：添加路由守卫跳过广告标记

--- src/component/welcome/FirstActions.tsx
+++ src/component/welcome/FirstActions.tsx
@@ -1,11 +1,13 @@
 import s from './welcome.module.scss';
-import { RouterLink } from 'vue-router';
-import { FunctionalComponent } from 'vue';
+import {RouterLink} from 'vue-router';
+import {FunctionalComponent} from 'vue';
+import {SkipFeatures} from "../../shared/SkipFeatures";
+
 export const FirstActions: FunctionalComponent = () => {
     return <div class={s.actions}>
-        <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-        <RouterLink to="/welcome/2" >下一页</RouterLink>
-        <RouterLink to="/start" >跳过</RouterLink>
+        <SkipFeatures class={s.fake}/>
+        <RouterLink to="/welcome/2">下一页</RouterLink>
+        <SkipFeatures/>
     </div>
 }
 
