d47079c8df7aa8d5be236c7847cc0df47390ca33
origami <1071242996@qq.com>
2022-11-20 15:05:10 +0800

    feature:使用mock并且封装createTag实现随机生成

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -13,41 +13,31 @@ export const mockSession: Mock = (config) => {
 }
 
 export const mockTagIndex: Mock = (config) => {
+    let id = 0 //id初始为0
+    const createId = () => {
+        id += 1 //id自增
+        return id
+    }
+    const createTag = (n = 1, attrs?: any) =>
+        //声明一个长度为length的数组，
+        //然后去map他，map的过程中返回这个包含id，name等内容的对象
+        Array.from({length: n}).map(() => (
+            {
+                id: createId(),
+                name: faker.lorem.word(),//随机文字
+                sign: faker.internet.emoji(),//emoji
+                kind: config.params.kind,//从config里面拿到kind
+                ...attrs //覆盖
+            }
+        ))
+
     if (config.params.kind === 'expenses') {
         return [200, {
-            resources: [
-                {id: 1, name: '餐饮', sign: '￥', find: 'expenses'},
-                {id: 2, name: '打车', sign: '￥', find: 'expenses'},
-                {id: 3, name: '地铁', sign: '￥', find: 'expenses'},
-                {id: 4, name: '公交', sign: '￥', find: 'expenses'},
-                {id: 5, name: '房租', sign: '￥', find: 'expenses'},
-                {id: 6, name: '游玩', sign: '￥', find: 'expenses'},
-                {id: 7, name: '聚餐', sign: '￥', find: 'expenses'},
-                {id: 8, name: '购物', sign: '￥', find: 'expenses'},
-                {id: 9, name: '生活', sign: '￥', find: 'expenses'},
-                {id: 10, name: '公益', sign: '￥', find: 'expenses'},
-                {id: 11, name: '保险', sign: '￥', find: 'expenses'},
-                {id: 12, name: '医疗', sign: '￥', find: 'expenses'},
-                {id: 13, name: '宠物', sign: '￥', find: 'expenses'},
-                {id: 14, name: '还款', sign: '￥', find: 'expenses'},
-                {id: 15, name: '游戏', sign: '￥', find: 'expenses'},
-            ]
+            resources: createTag(12)//随机生成12个tag
         }]
     } else {
         return [200, {
-            resources: [
-                {id: 16, name: '工资', sign: '￥', find: 'income'},
-                {id: 17, name: '彩票', sign: '￥', find: 'income'},
-                {id: 18, name: '基金', sign: '￥', find: 'income'},
-                {id: 19, name: '债券', sign: '￥', find: 'income'},
-                {id: 20, name: '定期', sign: '￥', find: 'income'},
-                {id: 21, name: '外汇', sign: '￥', find: 'income'},
-                {id: 22, name: '股票', sign: '￥', find: 'income'},
-                {id: 23, name: '贵金属', sign: '￥', find: 'income'},
-                {id: 24, name: '虚拟货币', sign: '￥', find: 'income'},
-                {id: 25, name: '转账', sign: '￥', find: 'income'},
-                {id: 26, name: '红包', sign: '￥', find: 'income'},
-            ]
+            resources: createTag(12)
         }]
     }
 }
