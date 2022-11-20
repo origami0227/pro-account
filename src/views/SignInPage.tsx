9485e3d8990c973622466b244016cb0346109d18
origami <1071242996@qq.com>
2022-11-20 13:43:46 +0800

    删除mock测试代码

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -36,7 +36,7 @@ export const SignInPage = defineComponent({
                 {key: 'code', type: 'required', message: '必填'},
             ]))
             if (!hasError(errors)) {
-                const response = await http.post<{ jwt: string }>('/session', formData,{params:{_mock:'session'}}).catch(onError)//要mock这个响应，mock的名字是session
+                const response = await http.post<{ jwt: string }>('/session', formData).catch(onError)//要mock这个响应，mock的名字是session
                 localStorage.setItem('jwt', response.data.jwt)
                 console.log(response)
                 //使用查询参数的思路
