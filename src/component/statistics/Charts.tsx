f109508c192cde5f82d4767303ac60762dad9a33
origami <1071242996@qq.com>
2022-11-28 13:21:54 +0800

    feature:数据修剪

--- src/component/statistics/Charts.tsx
+++ src/component/statistics/Charts.tsx
@@ -5,10 +5,11 @@ import {LineChart} from './LineChart';
 import {PieChart} from './PieChart';
 import {Bars} from './Bars';
 import {http} from "../../shared/Http";
+import {Time} from "../../shared/time";
 
 type Data1Item = {happen_at:string, amount: number}
 type Data1 = Data1Item[]
-
+const DAY = 24 * 3600 * 1000 //常量 一天的毫秒数
 export const Charts = defineComponent({
     props: {
         startDate: {
@@ -23,11 +24,30 @@ export const Charts = defineComponent({
     setup: (props, context) => {
         const kind = ref('expenses')
         const data1 = ref<Data1>([]) //data容器
-        const betterData1 = computed(()=> { //{happen_at , amount}
-            return data1.value.map(item=>
-                //对象转换成数组
-                [item.happen_at, item.amount] as [string, number]
-            )
+        const betterData1 = computed<[string, number][]>(()=> {
+            //判断
+            if(!props.startDate || !props.endDate) {
+                return []
+            }
+            //声明一个空数组，然后向这个数组里面push进一个月的数据
+            const array = []
+            //diff算出这个月有多少天 用结束时间戳减去开始时间戳算出秒
+            const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
+            //n等于 diff算出的秒除以一天的秒数➕1得到天数
+            const n = diff / DAY + 1
+            let data1Index = 0
+            for(let i=0; i<n; i++) {
+                //格式要求
+                const time = new Time(props.startDate+'T00:00:00.000+0800').add(i, 'day').getTimestamp()
+                if(data1.value[data1Index] && new Date(data1.value[data1Index].happen_at).getTime() === time){
+                    //放进空数组
+                    array.push([new Date(time).toISOString(), data1.value[data1Index].amount])
+                    data1Index += 1
+                }else{
+                    array.push([new Date(time).toISOString(), 0])
+                }
+            }
+            return array as [string, number][]
         })
 
         onMounted(async ()=>{
