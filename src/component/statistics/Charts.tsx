9f7f8b27c862b9137cd3009f9eb3bd24a5cab5c9
origami <1071242996@qq.com>
2022-11-28 14:45:06 +0800

    feature：优化条形图

--- src/component/statistics/Charts.tsx
+++ src/component/statistics/Charts.tsx
@@ -7,7 +7,7 @@ import {Bars} from './Bars';
 import {http} from "../../shared/Http";
 import {Time} from "../../shared/time";
 
-type Data1Item = {happen_at:string, amount: number}
+type Data1Item = { happen_at: string, amount: number }
 type Data1 = Data1Item[] //折线图数据
 type Data2Item = { tag_id: number; tag: Tag; amount: number }
 type Data2 = Data2Item[] // 饼图数据
@@ -27,14 +27,16 @@ export const Charts = defineComponent({
         const kind = ref('expenses')
         // data1 折线图
         const data1 = ref<Data1>([]) //data容器
-        const betterData1 = computed<[string, number][]>(()=> {
-            if(!props.startDate || !props.endDate) { return [] }
+        const betterData1 = computed<[string, number][]>(() => {
+            if (!props.startDate || !props.endDate) {
+                return []
+            }
             const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
             const n = diff / DAY + 1
             //Array.from({length:n})这种声明方法是有key的也就是有下标，然后进行map
             //否则需要fill
-            return Array.from({length: n}).map((_, i)=>{
-                const time = new Time(props.startDate+'T00:00:00.000+0800').add(i, 'day').getTimestamp()
+            return Array.from({length: n}).map((_, i) => {
+                const time = new Time(props.startDate + 'T00:00:00.000+0800').add(i, 'day').getTimestamp()
                 const item = data1.value[0]
                 const amount = (item && new Date(item.happen_at).getTime() === time)
                     ? data1.value.shift()!.amount //拿到data1的第一项
@@ -43,9 +45,9 @@ export const Charts = defineComponent({
             })
         })
 
-        onMounted(async ()=>{
+        onMounted(async () => {
             //挂载时请求记账数据
-            const response = await http.get<{groups: Data1, summary: number}>('/items/summary',{
+            const response = await http.get<{ groups: Data1, summary: number }>('/items/summary', {
                 happen_after: props.startDate, //开始时间
                 happen_before: props.endDate, //结束时间
                 kind: kind.value, //收入或者支出
@@ -63,9 +65,9 @@ export const Charts = defineComponent({
                 value: item.amount
             }))
         )
-        onMounted(async ()=>{
+        onMounted(async () => {
             //请求饼图数据
-            const response =  await http.get<{ groups: Data2; summary: number }>('/items/summary', {
+            const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
                 happen_after: props.startDate,
                 happen_before: props.endDate,
                 kind: kind.value,
@@ -74,7 +76,15 @@ export const Charts = defineComponent({
             })
             data2.value = response.data.groups
         })
+        //  data3
 
+        const betterData3 = computed<{ tag: Tag, amount: number, percent: number }[]>(() => {
+            const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
+            return data2.value.map(item => ({
+                ...item,
+                percent: Math.round(item.amount / total * 100)
+            }))
+        })
         return () => (
             <div class={s.wrapper}>
                 <FormItem label="类型" type="select" options={[
@@ -84,7 +94,7 @@ export const Charts = defineComponent({
                           v-model={kind.value}/>
                 <LineChart data={betterData1.value}/>
                 <PieChart data={betterData2.value}/>
-                <Bars/>
+                <Bars data={betterData3.value}/>
             </div>
         )
     }
