81328ec4b2501a983320f860685f1e4c6025475a
origami <1071242996@qq.com>
2022-12-03 13:20:27 +0800

    fix:修复错误请求下一页item的bug

--- src/stores/useItemStore.tsx
+++ src/stores/useItemStore.tsx
@@ -1,5 +1,5 @@
-import { defineStore } from 'pinia'
-import { http } from '../shared/Http'
+import {defineStore} from 'pinia'
+import {http} from '../shared/Http'
 
 type State = {
     items: Item[]
@@ -8,8 +8,11 @@ type State = {
 }
 type Actions = {
     reset: () => void
+    _fetch: (firstPage: boolean, startDate?: string, endDate?: string) => void
     fetchItems: (startDate?: string, endDate?: string) => void
+    fetchNextPage: (startDate?: string, endDate?: string) => void
 }
+
 export const useItemStore = (id: string | string[]) =>
     defineStore<string, State, {}, Actions>(typeof id === 'string' ? id : id.join('-'), {
         state: () => ({
@@ -25,7 +28,8 @@ export const useItemStore = (id: string | string[]) =>
                 this.page = 0
             },
             //传来startDate，endDate
-            async fetchItems(startDate, endDate) {
+            //封装fetch
+            async _fetch(firstPage, startDate, endDate) {
                 //条件判断，如果不存在起始和终止时间就直接返回
                 if (!startDate || !endDate) {
                     return
@@ -35,7 +39,7 @@ export const useItemStore = (id: string | string[]) =>
                     {
                         happen_after: startDate, //起始事件
                         happen_before: endDate, //结束事件
-                        page: this.page + 1
+                        page: firstPage ? 1 : this.page + 1 //页码判断
                     },
                     {
                         _mock: 'itemIndex', //mock数据
@@ -43,11 +47,22 @@ export const useItemStore = (id: string | string[]) =>
                     }
                 )
                 //析构赋值拿到resources和pager
-                const { resources, pager } = response.data
-                this.items?.push(...resources) //放入items里面
+                const {resources, pager} = response.data
+                //页码判断进行
+                if (firstPage) {
+                    this.items = resources //覆盖旧第一页数据
+                } else {
+                    this.items.push(...resources)
+                }
                 //计算下一页的方法
                 this.hasMore = (pager.page - 1) * pager.per_page + resources.length < pager.count
                 this.page += 1
+            },
+            async fetchNextPage(startDate, endDate) {
+                this._fetch(false, startDate, endDate) //请求下一页
+            },
+            async fetchItems(startDate, endDate) {
+                this._fetch(true, startDate, endDate) //仅仅请求第一页
             }
         }
     })()
