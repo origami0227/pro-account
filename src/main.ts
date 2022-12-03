9e0b53a489eeedd835c774903b0340120da50dc4
origami <1071242996@qq.com>
2022-12-03 13:54:24 +0800

    fix:解决登陆界面后退按钮的逻辑bug

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {history} from './shared/history'
 import '@svgstore';
 import {useMeStore} from "./stores/useMeStore";
 import {createPinia, storeToRefs} from "pinia";
+import {Dialog} from "vant";
 
 const router = createRouter({history, routes})
 const pinia = createPinia() //创建Pinia
@@ -38,7 +39,14 @@ router.beforeEach(async (to, from) => {
     }
     return mePromise!.value!.then(//beforeEach可以直接return一个promise
         () => true,//可以获取到用户信息
-        () => '/sign_in?return_to=' + to.path//false 获取不到用户信息再跳转到登陆界面
+
+        async () => {
+            await Dialog.alert({
+                title:'提示',
+                message:'请先登录'
+            })
+            return '/sign_in?return_to=' + from.path//false 获取不到用户信息再跳转到登陆界面
+        }
     )
 
 })
